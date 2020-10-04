696eacf8f4c00499d202d7ba50511002969ab883
CrystalNet Technologies <71816565+CrystalNet-Tech@users.noreply.github.com>
2020-10-04 19:43:28 +0100

    Update Program.java

--- src/ADONet/EntityClient/Program.java
+++ src/ADONet/EntityClient/Program.java
@@ -6,9 +6,6 @@ import system.data.CommandBehavior;
 import system.data.common.DbDataReader;
 import system.data.entityclient.*;
 
-/*
-The code in this example uses an EntityCommand to execute an Entity SQL query. This query returns a list of records that represent instances of the Categories entity type. An EntityDataReader is used to access data records in the result set. For more information, see EntityClient Provider for the Entity Framework.
- */
 public class Program {
 
     public static void main(String[] args) {
